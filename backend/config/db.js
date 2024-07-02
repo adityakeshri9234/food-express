479e77715723443c5e4c2c7ecfc1dac18d24f9c0
Aditya <adityakeshri9234@gmail.com>
2024-07-02 14:44:09 +0530

    second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 export const connectDB =async()=>{
     await mongoose.connect('mongodb+srv://adityakeshriofficial123:2ZZMw9QBleWxUqbj@cluster0.akb3rya.mongodb.net/food-express')
     .then(()=>console.log("db connected"))
